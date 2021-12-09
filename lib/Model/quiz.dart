44f0899c5cc3e31e7aa7613fa375b6bee883dbcb
Nathan Smith <smi00696@d.umn.edu>
2021-12-09 14:56:20 -0600

    Added helper methods to Quiz. Add more as needed.

--- lib/Model/quiz.dart
+++ lib/Model/quiz.dart
@@ -4,6 +4,13 @@ import 'question.dart';
 class Quiz {
   /// List of questions
   List<Question> questions = [];
+  /// Stores the current quiz question (indexes from 0)
+  int questionIndex = 0;
   /// Score on the quiz
   int score = 0;
+  /// Returns the current question in the quiz
+  Question getCurQuestion() {
+    return questions[questionIndex];
+  }
+
 }
