6bc3406a6dcbcafe5e17896554f718dae8af474d
Nathan Smith <smi00696@d.umn.edu>
2021-12-09 14:32:17 -0600

    Added question class

---
+++ lib/Model/question.dart
@@ -0,0 +1,14 @@
+/// Stores a question
+///
+/// String question: The question
+///
+/// List<String> answers: Possible answers for the question
+///
+/// String correct: The correct answer to the question
+class Question {
+  String question = "Placeholder?";
+
+  List<String> answers = [];
+
+  String correct = "Placeholder?";
+}
