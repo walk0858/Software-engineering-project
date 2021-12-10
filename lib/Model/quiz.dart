e11f07ae0a7c2c96d895c21f60c39e485ab11f25
Nathan Smith <smi00696@d.umn.edu>
2021-12-10 14:55:12 -0600

    Added constructor to Quiz

--- lib/Model/quiz.dart
+++ lib/Model/quiz.dart
@@ -8,6 +8,14 @@ class Quiz {
   int questionIndex = 0;
   /// Score on the quiz
   int score = 0;
+
+  /// Constructs a Quiz from a list of questions
+  ///
+  /// [questionList] is the list of questions to be included on the quiz.
+  Quiz(List<Question> questionList) {
+    questions = questionList;
+  }
+
   /// Returns the current question in the quiz
   Question getCurQuestion() {
     return questions[questionIndex];
